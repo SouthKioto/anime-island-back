4f396e1172b9cbeaf4ec4b9301701c243a18d532
Bartłomiej Bolisęga <bolisegabartek6@gmail.com>
2025-12-29 17:16:33 +0100

    dodanie clasy oAutch generujacej token

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
 <?php
 
 @include 'connect.php';
+@include 'oAutch/oAutch.php';
 
-//login, password 
+// TODO: Add oAutch token
 
 $login = $_POST['login'];
 $password = $_POST['password'];
@@ -13,7 +14,6 @@ if (empty($login) || empty($password)) {
   return;
 }
 
-
 $get_pass_query = "SELECT * FROM users WHERE nickname = '$login'";
 
 $results = $conn->query($get_pass_query);
@@ -21,8 +21,19 @@ $row = $results->fetch_object();
 
 $data_base_pass = $row->password_hash;
 
-if (password_verify($password, $data_base_pass)) {
-  echo json_encode(array('success' => "Good password"));
-}
+if (!password_verify($password, $data_base_pass)) {
+  http_response_code(400);
+  echo json_encode(array('error' => "Password are invalid"));
+  return;
+};
+
+$user_data = array(
+  'user_id' => $row->id,
+  'nickname' => $row->nickname,
+  'email' => $row->email,
+);
+
+
+echo json_encode(array('success' => "Successfully login", "user_data" => $user_data));
 
 $conn->close();
